c332cd0c7316a8958aa8de78760a6b5c0a1cbc0e
susmita sharma <ssusmita21@tbc.edu.np>
2026-05-05 13:20:06 +1000

    Update model selection in UI to only include ViT option

--- models/model_factory.py
+++ models/model_factory.py
@@ -1,10 +1,31 @@
 # will contains all the model factory classes
-
+# from resnet_model import ResNetModel
 from .vit_model import ViTModel
-from .vit_model import ViTModel as DeiTModel  # reuse same pipeline class
 from config.settings import AVAILABLE_MODELS
+from .text2image_model import Text2ImageModel
+
+
 
 def get_model(model_key: str):
     if model_key == "vit":
         return ViTModel(AVAILABLE_MODELS["vit"])
     
+    # text2image model
+    if model_key == "text2image":
+        return Text2ImageModel(AVAILABLE_MODELS["text2image"])
+    
+    
+    models = {
+        "vit": ViTModel,
+    # "resnet": ResNetModel,
+        "text2image": Text2ImageModel
+
+    }
+    
+    # error handling for unknown model key
+    if model_key in models:
+        raise ValueError(f"UJnknown model key: {model_key} . Available models are: {list(models.keys())}"
+    )
+
+
+    return models[model_key]()
